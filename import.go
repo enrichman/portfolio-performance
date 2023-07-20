96161b56df226d23bb2a29451a989d5f841a1231
Andrea Nanni <andrea.nanni@ditechspa.it>
2023-07-20 18:59:12 +0200

    add btp_valore_gn27

--- import.go
+++ import.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "github.com/enrichman/portfolio-perfomance/pkg/bond/btp/btp_italia_gn30"
 	_ "github.com/enrichman/portfolio-perfomance/pkg/bond/btp/btp_italia_mz28"
+	_ "github.com/enrichman/portfolio-perfomance/pkg/bond/btp/btp_valore_gn27"
 	_ "github.com/enrichman/portfolio-perfomance/pkg/equity/eurizon"
 	_ "github.com/enrichman/portfolio-perfomance/pkg/fp/fonte"
 )
